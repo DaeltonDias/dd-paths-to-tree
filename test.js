8339ac057055d02a78450ec4fdd0227b801d3376
daeltondias <daelton.dias@gmail.com>
2021-06-04 11:56:03 -0300

    Update

--- test.js
+++ test.js
@@ -12,6 +12,53 @@ const tree1 = pathsToTree(paths, options);
 const tree2 = pathsToTree(paths);
 
 console.log(tree1);
+// {
+//   'index.js': 'file',
+//   'src': {
+//     'lib': {
+//       'git.js': 'file',
+//       'server.js': 'file'
+//     },
+//   },
+//   'externs': {
+//     'jquery.js': 'file'
+//   }
+// }
 
 console.log(tree2);
-
+// [
+//   {
+//     name: 'src',
+//     type: 'folder',
+//     children: [
+//       {
+//         name: 'lib',
+//         type: 'folder',
+//         children: [
+//           {
+//             name: 'git.js',
+//             type: 'file'
+//           },
+//           {
+//             name: 'server.js',
+//             type: 'file'
+//           }
+//         ]
+//       }
+//     ]
+//   },
+//   {
+//     name: 'externs',
+//     type: 'folder',
+//     children: [
+//       {
+//         name: 'jquery.js',
+//         type: 'file'
+//       }
+//     ]
+//   },
+//   {
+//     name: 'index.js',
+//     type: 'file'
+//   }
+// ]
